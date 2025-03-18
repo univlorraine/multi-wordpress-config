a9b41487d025245e31ec8c0a3aef02730709bc44
Benjamin Lemoine <benjamin.lemoine@univ-lorraine.fr>
2025-03-18 11:04:43 +0100

    feat: add features and positions-by-role pods

---
+++ includes/pods/positions-by-role.php
@@ -0,0 +1,56 @@
+<?php
+
+// Sécurité : empêche l'accès direct au fichier.
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+$pod_name = 'positions_by_role';
+$pod_singular_name = 'position_by_role';
+
+return [
+    'pod_config' => [
+        'name' => $pod_name,
+        'label' => 'Positions par rôle',
+        'label_singular' => 'Position par rôle',
+        'label_add_new_item' => 'Nouvelle position par rôle',
+        'description' => 'Positions par rôle de l\'application Multi.',
+        'menu_position' => 11,
+        'menu_icon' => 'dashicons-sort',
+        'wpgraphql_singular_name' => $pod_singular_name,
+        'wpgraphql_plural_name' => $pod_name,
+        'options' => [
+            'singleton' => false,
+            // Indique quel champ sera utilisé comme titre dans l'interface d'administration (autrement affiche 'brouillon')
+            'title_field' => [$pod_singular_name . '_position', $pod_singular_name . '_role'],
+        ]
+    ],
+    'pod_fields' => [
+        $pod_singular_name . '_fields' => [
+            'label' => 'Champs Position par rôles',
+            'fields' => [
+                $pod_singular_name . '_position' => [
+                    'type' => 'number',
+                    'label' => 'Position d\'affichage',
+                    'required' => true,
+                    'description' => 'Ordre pour l\'affichage.',
+                    'number_max_length' => '-1',
+                ],
+                $pod_singular_name . '_role' => [
+                    'type' => 'pick',
+                    'label' => 'Rôle',
+                    'required' => true,
+                    'description' => 'Rôle de l\'utilisateur associé à la position.',
+                    'pick_object' => 'post_type',
+                    'pick_val' => 'roles',
+                    'pick_format_type' => 'single',
+                    'pick_format_single' => 'autocomplete',
+                    'pick_display_format_separator' => ', ',
+			        'pick_taggable' => '1',
+                    'simple_relationship' => '1',
+                    'pick_display_format_single' => 'name'
+                ],
+            ],
+        ]
+    ]
+];
